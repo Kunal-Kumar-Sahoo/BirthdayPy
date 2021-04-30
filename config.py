39ec6b4eb7b05338381226c1bf73ff2a6629295a
Kunal Kumar Sahoo <76040689+Kunal-Kumar-Sahoo@users.noreply.github.com>
2021-04-30 10:50:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Specify which file (without .py extension) in the arts folder should be used
+artFile = "example"
+# Speed of art
+speed = 0.005
+# Print code in the beginning
+codePrint = True
+codingSpeed = 0.01
+# Audio
+playAudio = True
+audio = 'test.mp3'
+# Random color is choosen from the list
+color = ['red','green','yellow','blue','magenta','cyan','white']
+# Change the keys of the dict to change the color codes
+# If you change the color codes for blink, remove blink(none) and random, you have to change it in pprint() function of main.py.py too.
+colorCodes = {'①':'grey','②':'red','③':'green','④':'yellow','⑤':'blue','⑥':'magenta','⑦':'cyan','⑧':'white','⑨':'blink','⑩':'none','®':'random'}
